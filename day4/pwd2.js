5f6b8d40185b2a00284e5835eca6f62fb294b65f
Rishikesh <rishikesh2511990@gmail.com>
2024-02-01 15:05:39 +0530

    commented for pwd

--- day4/pwd2.js
+++ day4/pwd2.js
@@ -6,17 +6,24 @@ for (let i = 0; i < pwdstr.length; i++) {
     currNum = pwdstr[i];
     if(currNum>=1 && currNum<=9){
         if (i == 0) {
+            //for first index = time 1
             time += 1;
         } else if(currNum==prevNum){
+            //for same num
             time+=0;
         }else {
+            // for adjacent or not
             if ((currNum % 3 == 0) && (currNum-1==prevNum || currNum+3==prevNum || currNum-3==prevNum)) {
+                //this is for last col
                 time+=1;
             } else if ((currNum % 3 == 1) && (currNum + 1 == prevNum || currNum + 3 == prevNum || currNum - 3 == prevNum)) {
+                //this is for first col
                 time+=1;
             } else if ((currNum % 3 == 2) && ((currNum - 1 == prevNum) || (currNum + 3 == prevNum) || (currNum - 3 == prevNum) || (currNum+1==prevNum))) {
+                //this is for sec col
                 time+=1;
             }else{
+                //this is the only non adjacent
                 time+=2;
             }
         }
