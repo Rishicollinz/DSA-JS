27bce873807db57cfc4d70f6ee3cea9285f6b478
Rishikesh <rishikesh2511990@gmail.com>
2024-02-05 13:10:54 +0530

    boat with comment

--- day6/lc881_boat.js
+++ day6/lc881_boat.js
@@ -1,5 +1,8 @@
 let people = [3,5,3,4], limit = 5;
 
+//a boat can atmost have 2 people within limit;
+//suppose if 2+3<5; we should rm 2 and 3 from arr, else if 2 person weight can't be added within limit, then add 1 person to the boat.
+
 people.sort((a,b)=>(b-a));
 // console.log(people);
 let curr,currlast;
