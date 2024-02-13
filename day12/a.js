7af84d046725e065d79fbfa5ab150f8780c65845
Rishikesh <rishikesh2511990@gmail.com>
2024-02-13 19:17:02 +0530

    day12 end

--- day12/a.js
+++ day12/a.js
@@ -1,17 +1,4 @@
+let names = ['iliakan', 'remy', 'jeresig'];
 
-class FormatError extends SyntaxError {
-    constructor(message) {
-        super(message);
-        this.message = "Formatting Error";
-        this.name = "FormatError";
-        this.stack = "stack";
-    }
-}
-let err = new FormatError("formatting error");
+let requests = names.map(name => fetch(`https://api.github.com/users/${name}`));
 
-console.log(err.message); // formatting error
-console.log(err.name); // FormatError
-console.log(err.stack); // stack
-
-console.log(err instanceof FormatError); // true
-console.log(err instanceof SyntaxError); // true (because inherits from SyntaxError)
